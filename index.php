e70596b1639398b4e2c4cc5a9ef7e032cd24f58c
David Indra <mail@davidindra.cz>
2017-12-02 19:53:59 +0100

    Changed sth...

--- index.php
+++ index.php
@@ -265,6 +265,29 @@
         &copy; 2017 David Indra, <a href="mailto:mail@davidindra.cz">mail@davidindra.cz</a>
     </section>
 
+    <?php
+
+    if(isset($_POST['submit'])){
+        /*if(!preg_match("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
+            $_POST['email'])){
+            ?>
+            <script type="text/javascript">
+                alert("E-mailová adresa je neplatná.");
+            </script>
+            <?php
+        }else{*/
+            mail("mail@davidindra.cz", "Zpráva z webu MDP od " . $_POST['name'] . "(" . $_POST['email'] . ")", $_POST['message']);
+
+            ?>
+            <script type="text/javascript">
+                alert("Zpráva byla úspěšně odeslána! :)");
+            </script>
+            <?php
+        //}
+    }
+
+    ?>
+
 </div>
 
 <div id="fb-root"></div>
