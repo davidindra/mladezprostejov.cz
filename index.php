7ffe7e547953e8f6fa15b37fa30b3ebedbb2155f
David Indra <mail@davidindra.cz>
2017-12-02 17:37:24 +0100

    Initial version

---
+++ index.php
@@ -0,0 +1,280 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Mládež děkanátu Prostějov</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&amp;subset=latin-ext" rel="stylesheet">
+    <script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
+
+    <script src="scripts.js"></script>
+</head>
+<body>
+
+<div id="container">
+    <section id="header" class="noborder">
+        <div id="logo">
+            <a href="/">
+                <img src="img/logo.png" alt="Mládež děkanátu Prostějov">
+            </a>
+        </div>
+
+        <div id="menu">
+            <ul>
+                <li><a href="#main">Úvod</a></li>
+                <li><a href="#calendar">Plán akcí</a></li>
+                <li><a href="#team">Animátoři</a></li>
+                <li><a href="#gallery">Galerie</a></li>
+                <li><a href="#message">Napiš nám!</a></li>
+            </ul>
+        </div>
+    </section>
+
+    <section id="banner" class="noborder">
+        <img src="img/heading.jpg" alt="Banner MDP">
+    </section>
+
+    <section id="main">
+        <h1>Úvod</h1>
+        <p>
+            Buď vítán na stránkách křesťanské mládeže prostějovského děkanátu. Tady jsi opravdu správně. I
+            zde jsi křesťanem. Najdeš tady mnoho užitečných informací o našich akcích a novinek ze života mladých
+            lidí, se kterými sdílíš stejnou víru, tak jak to na dobrých webovkách má být. Ale co je důležitější,
+            můžeš se zde setkat nejen se slovem lidským, ale i se slovem Božím, které proniká až do morku kostí
+            tomu, kdo se mu otevře.
+        </p>
+        <p>
+            Děkuji všem, kdo se podílíte na dobrém a užitečném obsahu těchto stránek. Ale také Vám, našim
+            čtenářům, věrným návštěvníkům a přispěvatelům. A se všemi se těším na osobní setkání při
+            některých z našich akcí.
+        </p>
+        <p>
+            o. Tomáš Strogan<br>
+            váš kaplan pro mládež
+        </p>
+    </section>
+
+    <section id="calendar">
+        <h1>Nejbližší akce</h1>
+
+        <div id="calendar-wrapper">
+            <table id="list">
+                <tr>
+                    <td>Crossna</td>
+                    <td>15. 12.</td>
+                </tr>
+                <tr>
+                    <td>Crossna</td>
+                    <td>5. 1.</td>
+                </tr>
+                <tr>
+                    <td>Crossna</td>
+                    <td>2. 2.</td>
+                </tr>
+                <tr>
+                    <td>Postní duchovní obnova</td>
+                    <td>17. 2.</td>
+                </tr>
+                <tr>
+                    <td>Crossna</td>
+                    <td>2. 3.</td>
+                </tr>
+                <tr>
+                    <td>Crossna</td>
+                    <td>6. 4.</td>
+                </tr>
+                <tr>
+                    <td>Crossna</td>
+                    <td>4. 5.</td>
+                </tr>
+                <tr>
+                    <td>Crossna</td>
+                    <td>1. 6.</td>
+                </tr>
+            </table>
+
+            <div id="fb">
+                <div class="fb-page" data-href="https://www.facebook.com/mladezprostejov"
+                     data-tabs="timeline" data-width="500" data-height="340"
+                     data-small-header="false" data-adapt-container-width="true"
+                     data-hide-cover="false" data-show-facepile="true">
+                    <blockquote cite="https://www.facebook.com/mladezprostejov"
+                                class="fb-xfbml-parse-ignore">
+                        <a href="https://www.facebook.com/mladezprostejov">
+                            Mládež děkanátu Prostějov
+                        </a>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="team">
+        <h1>Animátoři</h1>
+        
+        <div class="one">
+            <div class="header">
+                <img src="img/team/beata-hynkova.jpg" alt="">
+                <div class="name">
+                    <div class="first">Beáta</div>
+                    <div class="last">Hynková</div>
+                </div>
+            </div>
+            <p>
+                Ráda trávím čas se svými přáteli. Když je chvilka, tak ráda čtu, dívám se na filmy a zajdu do divadla. Taky poslouchám hudbu, maluji, bruslím a plavu.
+            </p>
+            <p>
+                Být animátor pro mě znamená znamená být tu pro druhé a jsem ráda, že mohu tímto prostřednictvím sloužit Bohu. Také jsem skrze tuto službu poznala nové přátele a užila si s nimi spoustu legrace. Jednoduše mě baví dělat něco, co má smysl, a přivádět mladé k Bohu smysl má.
+            </p>
+        </div>
+
+        <div class="one">
+            <div class="header">
+                <img src="img/team/jan-kapounek.jpg" alt="">
+                <div class="name">
+                    <div class="first">Jan</div>
+                    <div class="last">Kapounek</div>
+                </div>
+            </div>
+            <p>
+                Rád stříhám videa a fotím. Baví mě turistika - jak pěší, tak cyklo. K mým netypickým zálibám patří například autobusy.
+            </p>
+            <p>
+                Vždycky má smysl věnovat se mládeži. Je super, když se můžu podílet na vytváření party mladých tady, v děkanátu, jehož jsem součástí. Práce animátora mě zaujala už jako malého, když jsem jezdil na tábory s prostějovským klubem SHM.
+            </p>
+        </div>
+
+        <div class="one">
+            <div class="header">
+                <img src="img/team/andrea-batkova.jpeg" alt="">
+                <div class="name">
+                    <div class="first">Andrea</div>
+                    <div class="last">Baťková</div>
+                </div>
+            </div>
+            <p>
+                Mám ráda plaváni, jízdu na kole, přírodu. Baví mě hrát na kytaru a prostě být s lidmi.
+            </p>
+            <p>
+                Animátorkou jsem, protože jsem poznala sílu života s Ježíšem a chtěla bych to předávat dál. Myslim si, že je důležité, aby se věřící mládež scházela, sdílela se, diskutovala a modlila se spolu a jsem ráda, že jako animátor můžu být u toho.
+            </p>
+        </div>
+
+        <div class="one">
+            <div class="header">
+                <img src="img/team/tomas-pavelka.jpg" alt="">
+                <div class="name">
+                    <div class="first">Tomáš</div>
+                    <div class="last">Pavelka</div>
+                </div>
+            </div>
+            <p>
+                Rád trávím své volné chvíle s rodinou a přáteli. Zajímám se o veřejné dění, historii a technologické novinky. Sport není má silná stránka, badminton zvládám.
+            </p>
+            <p>
+                Být animátorem je hlavně velká příležitost. Mám na mysli příležitost k přátelství, společenství, zábavě a v neposlední řadě k nalézání a prohlubování naší víry.
+            </p>
+        </div>
+
+        <div class="one">
+            <div class="header">
+                <img src="img/team/tereza-pavlikova.jpg" alt="">
+                <div class="name">
+                    <div class="first">Tereza</div>
+                    <div class="last">Pavlíková</div>
+                </div>
+            </div>
+            <p>
+                Ráda čtu, jezdím na kole, sleduji filmy. Jednou bych chtěla navštívit Španělsko, Holandsko a Norsko. Ráda posedím s přáteli u sklenky vína.
+            </p>
+            <p>
+                Proč jsem animátorka? Ani nevím, zkrátka mě to baví. Ráda poznávám nové lidi a toto je skvělá příležitost. Také je potřeba určité zodpovědnosti.
+            </p>
+        </div>
+
+        <div class="one">
+            <div class="header">
+                <img src="img/team/vladimir-novotny.jpg" alt="">
+                <div class="name">
+                    <div class="first">Vladimír</div>
+                    <div class="last">Novotný</div>
+                </div>
+            </div>
+            <p>
+                Můj okruh zájmů a koníčků zahrnuje širokou oblast od četby klasických autorů, pres organizaci farního tábora až po příležitostné sportovní aktivity. Mimo tyto jsem fanouškem několika seriálů, pomocným včelařem, ministrantem a amatérským příznivcem origami.
+            </p>
+            <p>
+                Animátorem jsem se stal, jelikož mi dělal starosti stav křesťanské mládeže v našem děkanátu a já jsem cítil, že bych mohl pro druhé udělat něco navíc. Viděl jsem v tom také příležitost poznat nové lidi a získat zkušenosti s organizací společenských událostí.
+            </p>
+        </div>
+        
+    </section>
+
+    <section id="gallery">
+        <h1>Fotogalerie</h1>
+
+        <div class="tile">
+            <img src="img/galleries/1.jpg">
+            <div>Duchovní obnova 2017</div>
+        </div>
+
+        <div class="tile">
+            <img src="img/galleries/2.jpg">
+            <div>Církevní silvestr 2016</div>
+        </div>
+    </section>
+
+    <section id="message">
+        <h1>Napiš nám!</h1>
+
+        <form action="/" method="post">
+            <label>
+                Jméno a příjmení:<br>
+                <input type="text" name="name"><br>
+            </label>
+            <label>
+                E-mailová adresa pro odpověď:<br>
+                <input type="text" name="email"><br>
+            </label>
+            <label>
+                Zpráva:<br>
+                <textarea name="message"></textarea><br>
+            </label>
+            <input type="submit" name="submit" value="Odeslat zprávu">
+        </form>
+    </section>
+
+    <section id="contact">
+        <h1>Kontakt</h1>
+        <div class="one">
+            <div class="name">P. Tomáš Strogan</div>
+            <div class="phone">732 813 162</div>
+            <div class="email">kaplan@mladezprostejov.cz</div>
+        </div>
+        <div class="one">
+            <div class="name">Beáta Hynková</div>
+            <div class="phone">607 726 497</div>
+            <div class="email">beata.hynkova@mladezprostejov.cz</div>
+        </div>
+    </section>
+
+    <section id="copyright">
+        &copy; 2017 David Indra, <a href="mailto:mail@davidindra.cz">mail@davidindra.cz</a>
+    </section>
+
+</div>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v2.11&appId=1811210709152872';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+</body>
+</html>
---
+++ scripts.js
@@ -0,0 +1,8 @@
+// smooth scrolling
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
